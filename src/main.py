5a45fd503852b976f5b055c1fd78feb7a51afb53
BernardAmoah <51036551+BernardAmoah@users.noreply.github.com>
2024-04-18 15:20:28 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from train_models import *
+
+def main():
+
+    train_model('MLP', train_x, train_y)
+    #train_model('Decision_Tree', train_x, train_y)
+    #train_model('Naive_Bayes_classifier', train_x, train_y)
+    #train_model('Logistic_Regression', train_x, train_y)
+    #train_model('SVM', train_x, train_y)
+    #train_model('RandomForestModel', train_x, train_y)
+    #train_model('DecisionTreeClassifier', train_x, train_y)
+
+if __name__ == '__main__':
+    main()
